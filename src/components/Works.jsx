43f18ba9cab1cff2cd06e47e465e5df9f5df966b
Murtaza Sadriwala <msadriwala9@gmail.com>
2026-04-18 11:44:30 +0530

    fix: remove unused 'play' asset import in Works component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,7 @@
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { github, play } from "../assets";
+import { github} from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
